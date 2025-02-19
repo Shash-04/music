d84fe2fd783033e701a118ca6459ec85af132afc
Shashwat Vaish <91148757+Shash-04@users.noreply.github.com>
2025-02-19 18:56:37 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     domains: ['images.unsplash.com'],
+//   },
+//    eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+// };
+
+
+import { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   images: {
+    // Allow images to be loaded from Unsplash
     domains: ['images.unsplash.com'],
   },
-   eslint: {
+  eslint: {
+    // Disable ESLint during production builds
     ignoreDuringBuilds: true,
   },
 };
 
+export default nextConfig;
